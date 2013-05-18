aab5db270d40d609da505beda6434d7c83d72f67
psahni <prashant.sahni5@gmail.com>
2013-05-18 16:36:07 +0530

    initial commit - Mongo Mapper configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAppMongodb::Application.config.secret_token = '0852d4d6829dff7623005d89fa286c0e28b86a6d14ba13a1ebf1cdb0e338a7fb5751441a17a3edbb9130cf9821b44c358336ec4ce73962cb93c8c2df776fde47'
