dc6f021da27596838d2327fbd87138f041ee0a28
psahni <prashant.sahni5@gmail.com>
2013-05-19 21:22:41 +0530

    edit/update stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -5,7 +5,6 @@ class StoriesController < ApplicationController
   end
 
   def index
-    flash[:notice] = "test message test message"
     @stories = Story.all
   end
 
@@ -16,6 +15,25 @@ class StoriesController < ApplicationController
     else
       render :action => :new
     end
+  end
+  
+  
+  def edit
+    @story = Story.find(params[:id])
+  end
+  
+  
+  def update
+    @story = Story.find(params[:id])
+    if @story.update_attributes(params[:story])
+      redirect_to [:stories], :flash => {:notice => "Story has been successfully updated"}
+    else
+      render :action => :edit 
+    end
+  end
+  
+  def destroy
+    @story = Story.find(params[:id])
   end
 
 end
